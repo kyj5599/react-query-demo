e9cbf60bf2a154149b996f540bc2e172fa374216
kyj5599 <277k@naver.com>
2025-08-20 17:18:37 +0900

    ✨ 리액트 쿼리 API 호출

--- src/components/ReactQueryPage.jsx
+++ src/components/ReactQueryPage.jsx
@@ -1,15 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
 function ReactQueryPage() {
+  const fetchPost = () => {
+    return axios.get("http://localhost:3004/posts");
+  };
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPost,
+    // retry: 에러 시도 3번(총 4회)
+    retry: 1,
+    select: (data) => {
+      return data.data;
+    },
+  });
+  console.log("ddd", data, isLoading);
+  console.log("error", isError, error);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>React Query Demo</h1>
-      <p>This page demonstrates React Query functionality.</p>
-      <p>
-        You can implement data fetching, caching, and state management here.
-      </p>
+    <div>
+      {data.map((item) => (
+        <div>{item.title}</div>
+      ))}
     </div>
   );
+
+  // return (
+  //   <div style={{ padding: "20px" }}>
+  //     <h1>React Query Demo</h1>
+  //   </div>
+  // );
 }
 
 export default ReactQueryPage;
