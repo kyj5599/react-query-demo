03bf105b4a8be97df64596233fc4b555ee301a46
kyj5599 <277k@naver.com>
2025-08-22 14:55:13 +0900

    ✨ 커스텀 훅

--- src/components/ReactQueryPage.jsx
+++ src/components/ReactQueryPage.jsx
@@ -1,26 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import React from "react";
+import { usePostQuery } from "../hooks/usePosts";
 
 function ReactQueryPage() {
-  const fetchPost = (queryData) => {
-    const id = queryData.queryKey[1];
-    return axios.get(`http://localhost:3004/posts/${id}`);
-  };
-  const { isLoading, data, isError, error, refetch } = useQuery({
-    queryKey: ["posts", 1],
-    queryFn: fetchPost,
-    retry: 1,
-    // staleTime: 60000, // 1분(60초) // 기본값은 0
-    // gcTime: 10000, // 10초만 캐시가 유지 // stale < gctime
-    select: (data) => {
-      return data.data;
-    },
-    // enabled: false, // 기본값 true
-    // refetchInterval: 3000,
-    // refetchOnMount: false, // 기본값 true
-    // refetchOnWindowFocus: true,
-  });
+  const { data, isLoading, isError, error, refetch } = usePostQuery();
   console.log("ddd", data, isLoading);
   console.log("error", isError, error);
 
@@ -32,9 +14,9 @@ function ReactQueryPage() {
   }
   return (
     <div>
-      {/* {data?.map((item) => (
-        <div>{item.title}</div>
-      ))} */}
+      {data?.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
       <button onClick={refetch}>post 리스트 다시 들고오기</button>
     </div>
   );
